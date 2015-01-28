19fff49d9b186ba6e98b2e9b2d891c3df8caefcf
Nicolas Michel <nicolas.michel.lava@gmail.com>
2015-01-28 15:47:05 +0100

    * Update test gist.

--- validation_test.js
+++ validation_test.js
@@ -1,44 +1,58 @@
 var https = require('https'),
     jjpet = require('./lib/jjpet.js'),
-    assert = require('assert');
+    assert = require('assert'),
+    fs = require('fs');
 
-https.get("https://gist.githubusercontent.com/nmichel/8b0d6f194e89abb7281d/raw/5033cd42ce3bfbb4f291004737d31987969ef8ab/validation_tests.json",
+function doTests(allTests) {
+    var testCounter = 0,
+        someFailed = false;
+    for (var i = 0; i < allTests.length; ++i) {
+        var test = allTests[i],
+            p = test.pattern,
+            m = jjpet.compile(p);
+
+        for (var j = 0; j < test.tests.length; ++j) {
+            var t = test.tests[j],
+                r = jjpet.run(t.node, m, t.inject || {}),
+                node = JSON.stringify(t.node),
+                exp = {status: t.status, captures: t.captures},
+                res = false;
+
+            try {
+                assert.deepEqual(r, exp);
+                res = true;
+            }
+            catch (e) {
+                someFailed = true;
+            }
+
+            console.log('test ' + ++testCounter + '("' + p + '")'
+                + ' | ' + node
+                + ' | ' + JSON.stringify(exp) + ' | ' + (res ? 'PASS' : 'FAILED'));
+        }
+
+        assert.equal(someFailed, false, "Some tests failed");
+    }
+}
+
+https.get('https://gist.githubusercontent.com/nmichel/8b0d6f194e89abb7281d/raw/cbabb4fad5358540499ce5b399b04eecba6a210e/validation_tests.json',
     function(res) {
-        var testCounter = 0,
-            body = '';
+        var body = '';
+        res.setEncoding('utf8');
         res
             .on('data', function(chunk) {
                 body += chunk;
             })
             .on('end', function() {
-                var tests = JSON.parse(body),
-                    someFailed = false;
-                for (var i = 0; i < tests.length; ++i) {
-                    var test = tests[i],
-                        p = test.pattern,
-                        m = jjpet.compile(p);
-
-                    for (var j = 0; j < test.tests.length; ++j) {
-                        var t = test.tests[j],
-                            r = jjpet.run(t.node, m, t.inject || {}),
-                            node = JSON.stringify(t.node),
-                            exp = {status: t.status, captures: t.captures},
-                            res = false;
-
-                        try {
-                            assert.deepEqual(r, exp);
-                            res = true;
-                        }
-                        catch (e) {
-                            someFailed = true;
-                        }
-
-                        console.log('test ' + ++testCounter + '("' + p + '")'
-                            + ' | ' + node
-                            + ' | ' + JSON.stringify(exp) + ' | ' + (res ? 'PASS' : 'FAILED'));
-                    }
-
-                    assert.equal(someFailed, false, "Some tests failed");
-                }
+                doTests(JSON.parse(body));
             })
         });
+
+/*
+fs.readFile('/home/nmichel/projects/validation_tests.json.git/validation_tests.json', function(err, data) {
+    if (err) {
+        throw err; // <== 
+    }
+    doTests(JSON.parse(data));
+});
+*/
